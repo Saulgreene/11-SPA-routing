54dc1ae7ad0d59f170cb6e4e0478da6335e52ef9
Saul Greene <spdysg@gmail.com>
2017-03-28 16:40:34 -0700

    done with 5 todos

--- starter-code/public/scripts/controllers/routes.js
+++ starter-code/public/scripts/controllers/routes.js
@@ -1,6 +1,10 @@
 'use strict';
 
-// TODO: Configure routes for this app with page.js, by registering each URL your app can handle,
+// TODODONE: Configure routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
 
-// TODO: What function do you call to activate page.js? Fire it off now, to execute
+page('/', articleView.initIndexPage);
+page('/admin',adminView.initAdminPage);
+page('/new', newArticle.initNewArticlePage);
+// TODODONE: What function do you call to activate page.js? Fire it off now, to execute
+page();
